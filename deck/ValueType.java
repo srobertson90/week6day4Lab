1c50ec7eb81f1f9cf1849ef4fb50a22eb582ecfc
srobertson90 <stuart.robertson.applications@gmail.com>
2016-08-28 17:36:59 +0100

    added number values to card value

--- deck/ValueType.java
+++ deck/ValueType.java
@@ -2,19 +2,29 @@ package deck;
 
 public enum ValueType {
 
-  TWO,
-  THREE,
-  FOUR,
-  FIVE,
-  SIX,
-  SEVEN,
-  EIGHT,
-  NINE,
-  TEN,
-  JACK,
-  QUEEN,
-  KING,
-  ACE
+  ACE(1),
+  TWO(2),
+  THREE(3),
+  FOUR(4),
+  FIVE(5),
+  SIX(6),
+  SEVEN(7),
+  EIGHT(8),
+  NINE(9),
+  TEN(10),
+  JACK(10),
+  QUEEN(10),
+  KING(10)
+
+  int value;
+
+  ValueType(int value) {
+    this.value = value;
+  }
+
+  public int getValue(){
+    return value;
+  }
 
 
 }
